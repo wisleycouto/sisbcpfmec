7daf975b3a75c9a1062ef32ac52a1da6e6b6ca36
wisley <wisley.alves@gmail.com>
2021-05-19 23:11:05 -0300

    add

--- sisbcpfmec/app/Http/Controllers/HomeController.php
+++ sisbcpfmec/app/Http/Controllers/HomeController.php
@@ -10,7 +10,11 @@ class HomeController extends Controller
     public function index(Request $Request )
     {
     	$data =$Request->all();
+<<<<<<< HEAD
+    	//dd  ($data['username']);
+=======
     	dd  ($data['username']);
+>>>>>>> 5da83be948d99752fedcf00e1d1bf0725a93e506
     	return view('auth/welcome');
     }
 }
